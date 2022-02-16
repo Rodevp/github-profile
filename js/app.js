9869eee0ae978b4ecd8ec7e9d642fd2cd32916f6
Rodevp <rodriguezrodrigor12@gmail.com>
2022-02-16 17:29:29 -0500

    renderizando información

--- js/app.js
+++ js/app.js
@@ -1,8 +1,26 @@
 import { getUser } from './api.js'
+import { parseData } from './parseData.js'
+import {userInfo, userMoreInfo} from './render.js'
+
+const searchBtn = document.getElementById('btn_search')
 
 
-const searchUser = async () => {
-    //console.log(  await  getUser('Rodevp') )
-}
 
-searchUser()
+
+searchBtn.addEventListener('click', async (e) => {
+
+    const userSearch = document.getElementById('search')
+    const data = parseData( await getUser(userSearch.value) )
+
+    const root = document.getElementById('render_info')
+    const user = userInfo( data )
+    const  moreInfoUser = userMoreInfo( data )
+
+    root.innerHTML = ''
+
+    root.appendChild(user)
+    root.appendChild(moreInfoUser)
+    userSearch.value = ''
+    //console.log(parseData( await getUser('Rodevp') ) )
+
+})
--- js/render.js
+++ js/render.js
@@ -13,7 +13,7 @@ const userInfo = (userInfo) => {
 
     const divContentUser = createElement('div', 'content__user', '')
     const avatar = createElement('div', 'content__avatar', '')
-    const imgAvatar = createElement('div', 'content__avatar', '')
+    const imgAvatar = createElement('img', 'content__avatar', '')
     const user = createElement('h2', 'item', userInfo.user)
     const userName = createElement('p', 'item', userInfo.userName)
 
@@ -24,7 +24,6 @@ const userInfo = (userInfo) => {
     divContentUser.appendChild(user)
     divContentUser.appendChild(userName)
 
-
     return divContentUser
 
 }
@@ -32,10 +31,9 @@ const userInfo = (userInfo) => {
 const userMoreInfo = (userInfo) => {
 
     const divContentMoreInfo = createElement('div', 'content__more_information', '')
-    const bio = createElement('p', 'bio', `Repositorios públicos: ${userInfo.bio}`)
+    const bio = createElement('p', 'bio', `${userInfo.bio}`)
     const reposCount = createElement('p', 'item__more-info', `Repositorios públicos: ${userInfo.repos}`)
     const followers = createElement('p', 'item__more-info', `Seguidores: ${userInfo.followers}`)
-
 
     divContentMoreInfo.appendChild(bio)
     divContentMoreInfo.appendChild(reposCount)
